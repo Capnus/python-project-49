72962da45b3c8fd2ff7ea584534cd604f7de4d56
qwerty <Kehsa754@gmail.com>
2024-06-12 14:56:28 +0300

    3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+from brain_games import cli
 
 def greet():
-    return 'Welcome to the Brain Games!'
+    print('Welcome to the Brain Games!')
 
 def main():
-    print(greet())
+    greet()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
