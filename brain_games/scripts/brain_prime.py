3e42dd3367318de00fe7daa4bc2ac1f8a6a50d42
qwerty <Kehsa754@gmail.com>
2024-06-23 02:00:17 +0300

    prime

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from brain_games.games.is_prime import launch_game, rule
+from brain_games.games.core import core_games, greet
+
+
+def main():
+    greet()
+    print(rule())
+    while core_games(launch_game()) != 'end':
+        a = 'zaglushka'
+
+
+if __name__ == '__main__':
+    main()
