2003c6d9d15e776eb67930b28a2351fc5bc5c512
qwerty <Kehsa754@gmail.com>
2024-06-23 02:53:10 +0300

    even

--- brain_games/scripts/even_game.py
+++ brain_games/scripts/even_game.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-from brain_games.cli import welcome_user
 from brain_games.games.is_even import launch_game, rule
-from brain_games.scripts.brain_games import greet
-from brain_games.games.core import core_games
-
-rule = rule()
+from brain_games.games.core import core_games, greet
 
 
 def main():
     greet()
-    launch_game()
-    core_games()
+    print(rule())
+    while core_games(launch_game()) != 'end':
+        a = 'zaglushka'
 
 
 if __name__ == '__main__':
     main()
+  
