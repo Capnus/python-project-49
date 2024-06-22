efbab25483b96fac75807c3625e97b25c4e7319a
qwerty <Kehsa754@gmail.com>
2024-06-23 01:50:14 +0300

    right name

--- brain_games/games/core.py
+++ brain_games/games/core.py
@@ -6,7 +6,7 @@ name = ''
 def greet():
     print('Welcome to the Brain Games!')
     global name
-    name = 'Nick'
+    name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
 
 
