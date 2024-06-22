1a4fba5d34c8a0f22ca4d4541cb9b5fe68519ff5
qwerty <Kehsa754@gmail.com>
2024-06-22 23:54:27 +0300

    Calc

--- brain_games/games/core.py
+++ brain_games/games/core.py
@@ -1,23 +1,23 @@
 import prompt
 
-for _ in range(3):
-    def core_games(launch_game):
-        print('Welcome to the Brain Games!')
-        name = prompt.string('May I have your name? ')
-        print(f'Hello, {name}!')
-        count_correct_answers = 0
+
+def core_games(launch_game):
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    count_correct_answers = 0
+    while True:
         question, correct_answer = launch_game
-        while True:
-            if count_correct_answers == 3:
-                print(f'Congratulations, {name}!')
-                break
-            print('Question: ', question)
-            answer = input()
-            print('Your answer: ', answer)
-            if answer == correct_answer:
-                print('Correct!')
-                count_correct_answers += 1
-            else:
-                print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-                print(f"Let's try again, {name}!")
-                break
+        if count_correct_answers == 3:
+            print(f'Congratulations, {name}!')
+            break
+        print('Question: ', question)
+        answer = input()
+        print('Your answer: ', answer)
+        if answer == correct_answer:
+            print('Correct!')
+            count_correct_answers += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            break
