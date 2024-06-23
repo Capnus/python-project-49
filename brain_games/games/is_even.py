c09e06556f1f50851fd75cd8bbbcd4279271495d
qwerty <Kehsa754@gmail.com>
2024-06-23 03:02:01 +0300

    right even

--- brain_games/games/is_even.py
+++ brain_games/games/is_even.py
@@ -1,18 +1,14 @@
 from random import randint
 
+
 def rule():
-    return 'Answer "yes" if given number is even. Otherwise answer "no".'
+    return 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def launch_game():
-    question = randint(1, 99)
-    count_divisors = 0
-    for i in range(2, (question + 1) // 2):
-        if question % i == 0:
-            count_divisors += 1
-    if count_divisors == 0:
+    question = randint(1, 100)
+    if question % 2 == 0:
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
     return str(question), correct_answer
-
