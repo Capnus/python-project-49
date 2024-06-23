920abb2f106234654ee5ab985472ae5874f67287
qwerty <Kehsa754@gmail.com>
2024-06-23 03:09:52 +0300

    right NOD

--- brain_games/games/NOD.py
+++ brain_games/games/NOD.py
@@ -11,10 +11,10 @@ def launch_game():
     question = str(a) + ' ' + str(b)
     divisor_a = []
     divisor_b = []
-    for i in range(1, (a + 1) // 2):
+    for i in range(1, ((a + 1) // 2) + 1):
         if a % i == 0:
             divisor_a.append(i)
-    for i in range(1, (b + 1) // 2):
+    for i in range(1, ((b + 1) // 2) + 1):
         if b % i == 0:
             divisor_b.append(i)
     common_divisor = list(set(divisor_a) & set(divisor_b))
