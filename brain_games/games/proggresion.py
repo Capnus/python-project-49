3e505b87ce658d2ce585c33c347db73ddc2abb4f
qwerty <Kehsa754@gmail.com>
2024-07-04 11:43:45 +0300

    fix scripts

--- brain_games/games/proggresion.py
+++ brain_games/games/proggresion.py
@@ -4,6 +4,7 @@ RULE = 'What number is missing in the progression?'
 START_RANGE = 1
 END_RANGE = 100
 END_RANGE_STEP = 5
+END_RANGE_INDEX = 9
 
 
 def launch_game():
@@ -11,8 +12,7 @@ def launch_game():
     question = [randint(START_RANGE, END_RANGE)]
     for i in range(10):
         question.append(question[i] + step_list)
-
-    random_index = randint(0, 9)
+    random_index = randint(START_RANGE, END_RANGE_INDEX)
     correct_answer = question.pop(random_index)
     question.insert(random_index, '..')
     question = ' '.join(str(num) for num in question)
